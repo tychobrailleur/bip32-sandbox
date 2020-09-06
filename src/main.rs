948047e54228ef37c5e1fd3b0807b98db123c5c0
Sébastien Le Callonnec <sebastien@weblogism.com>
2020-09-06 14:41:12 +0100

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use hdwallet::{ExtendedPrivKey, ExtendedPubKey, KeyIndex};
+use ed25519_bip32::{XPrv, DerivationScheme};
+
+fn main() {
+
+    let master_key = ExtendedPrivKey::random().unwrap();
+
+    let normal_key_index = KeyIndex::Normal(0);
+    let normal_child_priv_key = master_key.derive_private_key(normal_key_index).unwrap();
+
+    let normal_child_public_key = ExtendedPubKey::from_private_key(&normal_child_priv_key);
+    let derived_public_key = ExtendedPubKey::derive_public_key(&normal_child_public_key, KeyIndex::Normal(1000000));
+
+    let normal_derived_private_key = normal_child_priv_key.derive_private_key(KeyIndex::Normal(1000000)).unwrap();
+    let normal_derived_public_key = ExtendedPubKey::from_private_key(&normal_derived_private_key);
+
+
+    println!("{:x?}", derived_public_key);
+    println!("{:x?}", normal_derived_public_key);
+
+
+    let bytes = [1u8; 96];
+    let xprv = XPrv::normalize_bytes_force3rd(bytes);
+
+    let xpriv_private_derived = xprv.derive(DerivationScheme::V2, 1000000);
+    let xpub_from_xpriv = xprv.public();
+
+    let xpub_derived_from_xpriv_derived = xpriv_private_derived.public();
+    let xpub_derived_from_xpub_dervied = xpub_from_xpriv.derive(DerivationScheme::V2, 1000000);
+
+    println!("{:x?}", xpub_derived_from_xpub_dervied);
+    println!("{:x?}", xpub_derived_from_xpriv_derived);
+
+}
